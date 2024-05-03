0f62d16e95b9712e16a26056062a03e033f7aed0
fernandosevilla <fernandosevilla028@gmail.com>
2024-05-03 12:40:43 +0200

    queda ofimueble

--- BD-JARDINERIAyNBA/pract5/funciones.sql
+++ BD-JARDINERIAyNBA/pract5/funciones.sql
@@ -24,7 +24,9 @@ BEGIN
 END
 $$
 
-UPDATE clientes SET LimiteCredito = 5000 WHERE CodigoCliente = 1;
+UPDATE clientes
+SET LimiteCredito = 5000 
+WHERE CodigoCliente = 1;
 
 -- 2
 DROP TRIGGER IF EXISTS asignacion2;
@@ -43,22 +45,86 @@ VALUES
 -- 3
 DROP TRIGGER IF EXISTS actualizarStock;
 DELIMITER $$
-CREATE TRIGGER actualizarStock BEFORE INSERT ON pedidos FOR EACH ROW
+CREATE TRIGGER actualizarStock BEFORE INSERT ON detallepedidos FOR EACH ROW
 BEGIN
-	DECLARE stock SMALLINT;
-    
-    
-    
-    
+    DECLARE stock SMALLINT;
+
+    SELECT CantidadEnStock INTO stock
+    FROM productos
+    WHERE CodigoProducto = NEW.CodigoProducto;
+
+    UPDATE productos
+    SET CantidadEnStock = stock - NEW.Cantidad
+    WHERE CodigoProducto = NEW.CodigoProducto;
 END
 $$
 
+INSERT INTO detallepedidos (CodigoPedido, CodigoProducto, Cantidad, PrecioUnidad, NumeroLinea)
+VALUES (1, 'AR-001', 5, 10.99, 1);
 
+-- 4
+DROP TRIGGER IF EXISTS reponerStock;
+DELIMITER $$
+CREATE TRIGGER reponerStock BEFORE DELETE ON detallepedidos FOR EACH ROW
+BEGIN
+	DECLARE stock SMALLINT;
 
+    SELECT CantidadEnStock INTO stock
+    FROM productos
+    WHERE CodigoProducto = OLD.CodigoProducto;
+    
+	UPDATE productos
+    SET CantidadEnStock = stock + OLD.Cantidad
+    WHERE CodigoProducto = OLD.CodigoProducto;
+END
+$$
 
+DELETE FROM detallepedidos 
+WHERE CodigoPedido = 2
+AND NumeroLinea = 6;
 
+-- 5
+DROP TRIGGER IF EXISTS estadoPedido;
+DELIMITER $$
+CREATE TRIGGER estadoPedido BEFORE UPDATE ON pedidos FOR EACH ROW
+BEGIN
+	IF (OLD.Estado = "Pendiente" OR OLD.Estado IS NULL) AND NEW.FechaEntrega > OLD.FechaPedido THEN
+		SET NEW.Estado = "entregado";
+	END IF;
+END
+$$
 
+UPDATE pedidos
+SET FechaEntrega = '2008-03-18'
+WHERE CodigoPedido = 50;
 
+-- 6
+DROP TRIGGER IF EXISTS calcularLineasVenta;
+DELIMITER $$
+CREATE TRIGGER calcularLineasVenta BEFORE INSERT ON lineasventa FOR EACH ROW
+BEGIN
+	DECLARE precioLV INT;
+    
+    SELECT PVP INTO precioLV FROM productos
+    WHERE CODIGO = NEW.CODPROD;
+    
+    SET NEW.PRECIO = precioLV;
+    SET NEW.IMPORTELINEA = precioLV * NEW.CTD;
+END
+$$
+
+INSERT INTO lineasventa (NOVENTA, CODPROD, CTD)
+VALUES (50, 2, 5);
+
+-- 7
+DROP TRIGGER IF EXISTS actualizaImporte;
+DELIMITER $$
+CREATE TRIGGER actualizaImporte BEFORE INSERT ON ventas FOR EACH ROW
+BEGIN
+	UPDATE 
+    
+END
+$$
 
 
 
